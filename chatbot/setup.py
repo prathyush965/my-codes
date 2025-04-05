2e02699b1fbce0eb5ee9567469ddcaf1a913e28c
prathyush965 <prathyush96@gmail.com>
2025-04-05 12:38:20 +0530

    chatbot

---
+++ chatbot/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medibot",
+    version="0.0.0",
+    author="prathyush",
+    author_email="prathyush965@gmail.com",
+    packages=find_packages(),
+    install_requires = [] 
+)
